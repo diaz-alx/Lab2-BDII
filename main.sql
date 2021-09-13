39e9509bdf41abb4862563ccbcd3cea5482bc155
diaz-alx <gabriel.diaz4@utp.ac.pa>
2021-09-12 23:46:28 -0500

    main update

--- main.sql
+++ main.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE clientes (
-    id_cliente NUMBER NOT NULL,
-    cedula     VARCHAR2(10) NOT NULL,
-    nombre1    VARCHAR2(45) NOT NULL,
-    apellido1  VARCHAR2(45) NOT NULL,
-    fecha_nac  DATE NOT NULL,
-    cod_profesion  NUMBER NOT NULL,
-    direccion  VARCHAR2(250) NOT NULL,
-    sexo       VARCHAR2(2) NOT NULL
+    id_cliente    NUMBER NOT NULL,
+    cedula        VARCHAR2(10) NOT NULL,
+    nombre1       VARCHAR2(45),
+    apellido1     VARCHAR2(45),
+    fecha_nac     DATE,
+    cod_profesion NUMBER NOT NULL,
+    direccion     VARCHAR2(250),
+    sexo          VARCHAR2(2)
 );
 
 ALTER TABLE clientes ADD CONSTRAINT clientes_pk PRIMARY KEY ( id_cliente );
@@ -21,14 +21,6 @@ CREATE TABLE clientes_correos (
 ALTER TABLE clientes_correos ADD CONSTRAINT clientes_correos_pk PRIMARY KEY ( id_cliente,
                                                                               id_correo );
 
-CREATE TABLE clientes_prestamos (
-    id_cliente  NUMBER NOT NULL,
-    no_prestamo NUMBER NOT NULL
-);
-
-ALTER TABLE clientes_prestamos ADD CONSTRAINT clientes_prestamos_pk PRIMARY KEY ( id_cliente,
-                                                                                  no_prestamo );
-
 CREATE TABLE clientes_telefonos (
     id_cliente  NUMBER NOT NULL,
     id_telefono NUMBER NOT NULL,
@@ -40,7 +32,7 @@ ALTER TABLE clientes_telefonos ADD CONSTRAINT clientes_telefonos_pk PRIMARY KEY 
 
 CREATE TABLE pagos (
     no_pago      NUMBER NOT NULL,
-    monto_pagado NUMBER(6, 4) DEFAULT 0,
+    monto_pagado NUMBER(6, 4),
     fecha_pagado DATE
 );
 
@@ -56,10 +48,12 @@ ALTER TABLE pagos_prestamos ADD CONSTRAINT pagos_prestamos_pkv2 PRIMARY KEY ( no
 
 CREATE TABLE prestamos (
     no_prestamo    NUMBER NOT NULL,
-    monto_aprobado NUMBER(6, 4) DEFAULT 0,
-    fecha_aprobado DATE NOT NULL,
+    id_cliente     NUMBER NOT NULL,
     tipo_prestamo  NUMBER NOT NULL,
-    letra_mensual  NUMBER(6, 4) DEFAULT 0
+    fecha_aprobado DATE,
+    monto_aprobado NUMBER(6, 4),
+    no_coutas      NUMBER,
+    letra_mensual  NUMBER(6, 4)
 );
 
 ALTER TABLE prestamos ADD CONSTRAINT prestamos_pk PRIMARY KEY ( no_prestamo );
@@ -80,25 +74,21 @@ ALTER TABLE tipos_correos ADD CONSTRAINT tipos_correos_pk PRIMARY KEY ( cod_corr
 
 CREATE TABLE tipos_prestamos (
     cod_prestamo    NUMBER NOT NULL,
-    nombre_prestamo VARCHAR2(45) NOT NULL,
-    tasa_interes    NUMBER(2, 2) DEFAULT 0
+    nombre_prestamo VARCHAR2(45),
+    tasa_interes    NUMBER(2, 2)
 );
 
 ALTER TABLE tipos_prestamos ADD CONSTRAINT tipos_prestamos_pk PRIMARY KEY ( cod_prestamo );
 
 CREATE TABLE tipos_telefonos (
     cod_telefono NUMBER NOT NULL,
-    descripcion  VARCHAR2(10)
+    descripcion  VARCHAR2(50)
 );
 
 ALTER TABLE tipos_telefonos ADD CONSTRAINT tipos_telefonos_pk PRIMARY KEY ( cod_telefono );
 
 ALTER TABLE clientes_correos
     ADD CONSTRAINT clientes_fk FOREIGN KEY ( id_cliente )
-        REFERENCES clientes ( id_cliente );
-
-ALTER TABLE clientes_prestamos
-    ADD CONSTRAINT clientes_prest_fk FOREIGN KEY ( id_cliente )
         REFERENCES clientes ( id_cliente );
 
 ALTER TABLE clientes
@@ -121,9 +111,9 @@ ALTER TABLE pagos_prestamos
     ADD CONSTRAINT pagos_prestamos_prestamos_fk FOREIGN KEY ( no_prestamo )
         REFERENCES prestamos ( no_prestamo );
 
-ALTER TABLE clientes_prestamos
-    ADD CONSTRAINT prestamos_fk FOREIGN KEY ( no_prestamo )
-        REFERENCES prestamos ( no_prestamo );
+ALTER TABLE prestamos
+    ADD CONSTRAINT prestamos_clientes_fk FOREIGN KEY ( id_cliente )
+        REFERENCES clientes ( id_cliente );
 
 ALTER TABLE prestamos
     ADD CONSTRAINT prestamos_tipos_prestamos_fk FOREIGN KEY ( tipo_prestamo )

a7b25d42da20a9cfadb99d08e78222362583db0b
Fernando Cutire <fernandocutire@gmail.com>
2021-11-06 15:10:03 -0500

    PROGRESS: Triggers - inicio de triggers 2 y 3, correccion del trigger 1

--- triggers/trigger2.sql
+++ triggers/trigger2.sql
@@ -8,11 +8,25 @@ montos de los ahorros correspondientes.
 CREATE OR REPLACE TRIGGER AHORROS_CORRESPONDIENTES 
 
 -- Inicio de la sección declarativa
+AFTER UPDATE OF saldo_ahorro
+  ON AHORROS
+  FOR EACH ROW
 
 BEGIN
 -- Inicio de la sección ejecutable
+    IF to_char(CURRENT_DATE, 'dd') = '06' AND :NEW.tipo_ahorro = 2 
+      THEN
+       UPDATE TIPO_AH_SUC
+        SET monto_ahorros = monto_ahorros + :NEW.saldo_interes
+      WHERE COD_SUCURSAL = :NEW.COD_SUCURSAL;
+      ELSE
+       UPDATE SUCURSALES
+        SET monto_ahorros = monto_ahorros + :NEW.saldo_ahorro
+      WHERE COD_SUCURSAL = :NEW.COD_SUCURSAL;
+    END IF;
 
-EXCEPTION
+EXCEPTION WHEN dup_val_on_index THEN
+  null;
 -- Inicio de la sección de excepciones
 
 
--- triggers/trigger3.sql
+++ triggers/trigger3.sql
@@ -2,12 +2,43 @@
 Triggers. Para inserciones en la tabla de auditoria.
 */
 
+/*
+CREATE TABLE AUDITORIA (
+    id_auditoria NUMBER NOT NULL,
+    id_transaccion NUMBER NOT NULL,
+    id_cliente NUMBER NOT NULL,
+    id_tipo_ahorro NUMBER NOT NULL,
+    tipo_operacion CHAR NOT NULL,
+    tipo_transac NUMBER NOT NULL,
+    tabla VARCHAR2(25),
+    saldo_anterior NUMBER (15, 2),
+    monto_deposito NUMBER (15, 2),
+    saldo_final NUMBER (15, 2),
+    usuario VARCHAR2(42),
+    CONSTRAINT tipo_operacion_ck CHECK ( tipo_operacion IN ('I', 'U', 'D')),
+    CONSTRAINT auditoria_tipo_transac_ck CHECK ( tipo_transac IN(1, 2)),
+    CONSTRAINT auditoria_pk PRIMARY KEY (id_auditoria),
+    CONSTRAINT auditoria_transaDepoReti_fk FOREIGN KEY (id_transaccion)
+        REFERENCES transaDepoReti (id_transaccion),
+    CONSTRAINT auditoria_cliente_fk FOREIGN KEY (id_cliente)
+        REFERENCES clientes(id_cliente),
+    CONSTRAINT auditoria_tipo_ahorro_fk FOREIGN KEY (id_tipo_ahorro)
+        REFERENCES tipos_ahorros (id_tipo_ahorro)
+);
+
+
+*/
 
 CREATE OR REPLACE TRIGGER AUDITORIA 
 -- Inicio de la sección declarativa
+AFTER UPDATE OF saldo_ahorro
+  ON AHORROS
+  FOR EACH ROW
 
 BEGIN
 -- Inicio de la sección ejecutable
+  
+
 
 EXCEPTION
 -- Inicio de la sección de excepciones

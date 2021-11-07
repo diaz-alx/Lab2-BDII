ddf3fa2b3a4ce3a18bfe089b63a1dbc116f136d8
Fernando Cutire <fernandocutire@gmail.com>
2021-11-06 22:45:00 -0500

    PROGRESS: trigger3 - inicio de progreso de la tabla auditoria

--- triggers/trigger3.sql
+++ triggers/trigger3.sql
@@ -52,7 +52,17 @@ BEGIN
     saldo_anterior
     monto_deposito
     saldo_final
-    usuario 
+    usuario
+    UPDATE AUDITORIA
+        SET 
+        id_transaccion = :NEW.id_transaccion,
+        id_cliente = :NEW.id_cliente,
+        id_tipo_ahorro = :NEW.id_tipo_ahorro,
+        tipo_transac = :NEW.tipo_transac,
+        monto_deposito = :NEW.monto
+        usuario = :NEW.usuario
+        WHERE id_auditoria = :NEW.id_auditoria;
+        
 
 
     
@@ -60,3 +70,35 @@ EXCEPTION
 -- Inicio de la sección de excepciones
 
 END AUDITORIA;
+
+
+/* 
+CREATE TABLE transaDepoReti (
+    id_transaccion NUMBER NOT NULL,
+    no_cuenta NUMBER NOT NULL,
+    id_cliente NUMBER NOT NULL,
+    tipo_ahorro NUMBER NOT NULL,
+    cod_sucursal NUMBER NOT NULL,
+    fecha_transac DATE,
+    tipo_transac NUMBER,
+    monto NUMBER(15, 2) DEFAULT 0,
+    fecha_inserccion DATE,
+    status CHAR(2) NOT NULL,
+    usuario VARCHAR2(45),
+
+
+CREATE TABLE AUDITORIA (
+    id_auditoria NUMBER NOT NULL,
+    id_transaccion NUMBER NOT NULL,
+    id_cliente NUMBER NOT NULL,
+    id_tipo_ahorro NUMBER NOT NULL,
+    tipo_operacion CHAR NOT NULL,
+    tipo_transac NUMBER NOT NULL,
+    tabla VARCHAR2(25),
+    saldo_anterior NUMBER (15, 2),
+    monto_deposito NUMBER (15, 2),
+    saldo_final NUMBER (15, 2),
+    usuario VARCHAR2(42),
+    CONSTRAINT tipo_operacion_c
+    
+     */

5fea9d4a9ef3366a94048802fb37fd541e77caa7
Fernando Cutire <fernandocutire@gmail.com>
2021-11-06 22:14:25 -0500

    NEW: Trigger1y2 - trigger 1 y 2 completos

--- triggers/trigger2.sql
+++ triggers/trigger2.sql
@@ -5,7 +5,7 @@ montos de los ahorros correspondientes.
 */
 
 
-CREATE OR REPLACE TRIGGER AHORROS_CORRESPONDIENTES 
+CREATE OR REPLACE TRIGGER TRIGGER_2 
 
 -- Inicio de la sección declarativa
 AFTER UPDATE OF saldo_ahorro
@@ -16,18 +16,43 @@ BEGIN
 -- Inicio de la sección ejecutable
     IF to_char(CURRENT_DATE, 'dd') = '06' AND :NEW.tipo_ahorro = 2 
       THEN
+      IF :NEW.saldo_interes > 0 THEN
        UPDATE TIPO_AH_SUC
         SET monto_ahorros = monto_ahorros + :NEW.saldo_interes
-      WHERE COD_SUCURSAL = :NEW.COD_SUCURSAL;
+      WHERE COD_SUCURSAL = :NEW.COD_SUCURSAL
+      AND id_tipo_ahorro = :NEW.tipo_ahorro;
       ELSE
-       UPDATE SUCURSALES
+       UPDATE TIPO_AH_SUC
         SET monto_ahorros = monto_ahorros + :NEW.saldo_ahorro
-      WHERE COD_SUCURSAL = :NEW.COD_SUCURSAL;
+      WHERE COD_SUCURSAL = :NEW.COD_SUCURSAL
+      AND id_tipo_ahorro = :NEW.tipo_ahorro;
+      END IF;
+    ELSE
+      UPDATE TIPO_AH_SUC
+        SET monto_ahorros = monto_ahorros + :NEW.saldo_ahorro
+      WHERE COD_SUCURSAL = :NEW.COD_SUCURSAL
+      AND id_tipo_ahorro = :NEW.tipo_ahorro;
     END IF;
 
 EXCEPTION WHEN dup_val_on_index THEN
   null;
--- Inicio de la sección de excepciones
 
+END TRIGGER_2;
+/
 
-END AHORROS_CORRESPONDIENTES;
+drop trigger CARGATIPOAHORROSUC;
+BEGIN
+INSERT INTO TIPO_AH_SUC VALUES(1,1,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(1,2,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(1,3,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(2,1,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(2,2,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(2,3,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(3,1,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(3,2,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(3,3,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(4,1,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(4,2,0,SYSDATE);
+INSERT INTO TIPO_AH_SUC VALUES(4,3,0,SYSDATE);
+END;
+/
